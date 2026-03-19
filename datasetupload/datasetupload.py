aba88717529c2efa2bd55acd6e233159dac0aa73
sachincodes <sachinkarale2005@gmail.com>
2026-03-19 15:12:39 +0530

    singlefileuploaddone

--- datasetupload/datasetupload.py
+++ datasetupload/datasetupload.py
@@ -1,1 +1,14 @@
-# user can upload dataset in csv / excel form  - upto 100 mb .
+#FILE UPLOAD CODE 1 
+
+import streamlit as st # For file upload
+import pandas as pd 
+
+file = st.file_uploader("Upload CSV file" , type =["csv" , "xlsx"]) # streamlit has file_uploader lets you upload <200mb and any type of file 
+
+# user ----- CSV file as input . 
+
+
+#FILE UPLOAD ENDED 
+
+
+#Multiple files upload - multifile = st.file_uploader("upload CSV , XLSX" , type = ["csv" , "xlsx"] , accept_multiple_files = True )
